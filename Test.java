c184b3e3f94129791e1213b394a525dff42fe408
Ana Bajić <ana.bajic13@gmail.com>
2017-05-06 20:18:15 +0200

    final final gui version

--- Test.java
+++ Test.java
@@ -5,32 +5,32 @@ import javax.swing.*;
 public class Test {
 	 private JFrame mainFrame;
 	 private JTextField textField;
-	 private JTextField tfName;
-	 private JTextField tfSurname;
-	 private JTextField textField_1;
-	 private JTextField textField_2;
-	 private JTextField textField_3;
-	 private JTextField textField_4;
-	 private JTextField textField_5;
-	 private JTextField textField_6;
-	 private JTextField textField_7;
-	 private JTextField textField_8;
-	 private JTextField textField_9;
-	 private JTextField textField_10;
-	 private JTextField textField_11;
-	 private JTextField textField_12;
-	 private JTextField textField_13;
-	 private JTextField textField_14;
-	 private JTextField textField_15;
-	 private JTextField textField_16;
-	 private JTextField textField_17;
+	 private JTextField tfIme;
+	 private JTextField tfPrezime;
+	 private JTextField tfPol;
+	 private JTextField tfJmbg;
+	 private JTextField tfDatumRodjenja;
+	 private JTextField tfMestoRodjenja;
+	 private JTextField tfDrzavaRodjenja;
+	 private JTextField tfImeOca;
+	 private JTextField tfImeMajke;
+	 private JTextField tfUlica;
+	 private JTextField tfBroj;
+	 private JTextField tfMesto;
+	 private JTextField tfDrzava;
+	 private JTextField tfBrojTelefona;
+	 private JTextField tfBrojMobilnog;
+	 private JTextField tfEmail;
+	 private JTextField tfWww;
+	 private JTextField tfDatumUpisa;
+	 private JTextField tfSmer;
 	 private JLabel lblDrzavaRodjenja;
 	 private JLabel lblImeOca;
 	 private JLabel lblImeMajke;
 	 private JLabel lblUlica;
 	 private JLabel lblBoj;
 	 private JLabel lblMesto;
-	 private JTextField textField_20;
+	 private JTextField tfPostanskiBroj;
 	 private JLabel lblDrzava;
 	 private JLabel lblPostanskiBroj;
 	 private JLabel lblNewLabel_1;
@@ -48,12 +48,12 @@ public class Test {
 //	      swingControlDemo.showEventDemo();       
 	   }
 	   private void prepareGUI(){
-	      mainFrame = new JFrame("Java SWING Examples");
+	      mainFrame = new JFrame("Podaci o studentu");
 	      mainFrame.getContentPane().setBackground(new Color(175, 238, 238));
-	      mainFrame.setSize(775,615);
+	      mainFrame.setSize(742,586);
 	      mainFrame.getContentPane().setLayout(null);
 	      
-	      JLabel lblInfo = new JLabel("Unesite indeks studenta");
+	      JLabel lblInfo = new JLabel("Unesite broj indeksa studenta");
 	      lblInfo.setBounds(10, 11, 241, 23);
 	      mainFrame.getContentPane().add(lblInfo);
 	      
@@ -68,109 +68,109 @@ public class Test {
 	      mainFrame.getContentPane().add(lblError);
 	      
 	      JButton btnChoose = new JButton("Izaberi!");
-	      btnChoose.setBounds(392, 36, 89, 23);
+	      btnChoose.setBounds(566, 36, 89, 23);
 	      mainFrame.getContentPane().add(btnChoose);
 	      
 	      JPanel panel = new JPanel();
 	      panel.setBackground(new Color(175, 238, 238));
-	      panel.setBounds(10, 73, 739, 492);
+	      panel.setBounds(10, 73, 706, 463);
 	      mainFrame.getContentPane().add(panel);
 	      panel.setLayout(null);
 	      
-	      tfName = new JTextField();
-	      tfName.setBounds(10, 11, 195, 20);
-	      panel.add(tfName);
-	      tfName.setColumns(10);
+	      tfIme = new JTextField();
+	      tfIme.setBounds(10, 11, 195, 20);
+	      panel.add(tfIme);
+	      tfIme.setColumns(10);
 	      
-	      tfSurname = new JTextField();
-	      tfSurname.setBounds(10, 42, 195, 20);
-	      panel.add(tfSurname);
-	      tfSurname.setColumns(10);
+	      tfPrezime = new JTextField();
+	      tfPrezime.setBounds(10, 42, 195, 20);
+	      panel.add(tfPrezime);
+	      tfPrezime.setColumns(10);
 	      
-	      textField_1 = new JTextField();
-	      textField_1.setBounds(10, 73, 195, 20);
-	      panel.add(textField_1);
-	      textField_1.setColumns(10);
+	      tfPol = new JTextField();
+	      tfPol.setBounds(10, 73, 195, 20);
+	      panel.add(tfPol);
+	      tfPol.setColumns(10);
 	      
-	      textField_2 = new JTextField();
-	      textField_2.setBounds(10, 104, 195, 20);
-	      panel.add(textField_2);
-	      textField_2.setColumns(10);
+	      tfJmbg = new JTextField();
+	      tfJmbg.setBounds(10, 104, 195, 20);
+	      panel.add(tfJmbg);
+	      tfJmbg.setColumns(10);
 	      
-	      textField_3 = new JTextField();
-	      textField_3.setBounds(10, 135, 195, 20);
-	      panel.add(textField_3);
-	      textField_3.setColumns(10);
+	      tfDatumRodjenja = new JTextField();
+	      tfDatumRodjenja.setBounds(10, 135, 195, 20);
+	      panel.add(tfDatumRodjenja);
+	      tfDatumRodjenja.setColumns(10);
 	      
-	      textField_4 = new JTextField();
-	      textField_4.setBounds(10, 166, 195, 20);
-	      panel.add(textField_4);
-	      textField_4.setColumns(10);
+	      tfMestoRodjenja = new JTextField();
+	      tfMestoRodjenja.setBounds(10, 166, 195, 20);
+	      panel.add(tfMestoRodjenja);
+	      tfMestoRodjenja.setColumns(10);
 	      
-	      textField_5 = new JTextField();
-	      textField_5.setBounds(10, 197, 195, 20);
-	      panel.add(textField_5);
-	      textField_5.setColumns(10);
+	      tfDrzavaRodjenja = new JTextField();
+	      tfDrzavaRodjenja.setBounds(10, 197, 195, 20);
+	      panel.add(tfDrzavaRodjenja);
+	      tfDrzavaRodjenja.setColumns(10);
 	      
-	      textField_6 = new JTextField();
-	      textField_6.setBounds(10, 228, 195, 20);
-	      panel.add(textField_6);
-	      textField_6.setColumns(10);
+	      tfImeOca = new JTextField();
+	      tfImeOca.setBounds(365, 11, 195, 20);
+	      panel.add(tfImeOca);
+	      tfImeOca.setColumns(10);
 	      
-	      textField_7 = new JTextField();
-	      textField_7.setBounds(10, 254, 195, 20);
-	      panel.add(textField_7);
-	      textField_7.setColumns(10);
+	      tfImeMajke = new JTextField();
+	      tfImeMajke.setBounds(365, 42, 195, 20);
+	      panel.add(tfImeMajke);
+	      tfImeMajke.setColumns(10);
 	      
-	      textField_8 = new JTextField();
-	      textField_8.setBounds(10, 287, 195, 20);
-	      panel.add(textField_8);
-	      textField_8.setColumns(10);
+	      tfUlica = new JTextField();
+	      tfUlica.setBounds(365, 73, 195, 20);
+	      panel.add(tfUlica);
+	      tfUlica.setColumns(10);
 	      
-	      textField_9 = new JTextField();
-	      textField_9.setBounds(10, 318, 195, 20);
-	      panel.add(textField_9);
-	      textField_9.setColumns(10);
+	      tfBroj = new JTextField();
+	      tfBroj.setBounds(365, 104, 195, 20);
+	      panel.add(tfBroj);
+	      tfBroj.setColumns(10);
 	      
-	      textField_10 = new JTextField();
-	      textField_10.setBounds(10, 347, 195, 20);
-	      panel.add(textField_10);
-	      textField_10.setColumns(10);
+	      tfMesto = new JTextField();
+	      tfMesto.setBounds(365, 133, 195, 20);
+	      panel.add(tfMesto);
+	      tfMesto.setColumns(10);
 	      
-	      textField_11 = new JTextField();
-	      textField_11.setBounds(10, 380, 195, 20);
-	      panel.add(textField_11);
-	      textField_11.setColumns(10);
+	      tfDrzava = new JTextField();
+	      tfDrzava.setBounds(365, 166, 195, 20);
+	      panel.add(tfDrzava);
+	      tfDrzava.setColumns(10);
 	      
-	      textField_12 = new JTextField();
-	      textField_12.setBounds(365, 11, 195, 20);
-	      panel.add(textField_12);
-	      textField_12.setColumns(10);
+	      tfBrojTelefona = new JTextField();
+	      tfBrojTelefona.setBounds(10, 287, 195, 20);
+	      panel.add(tfBrojTelefona);
+	      tfBrojTelefona.setColumns(10);
 	      
-	      textField_13 = new JTextField();
-	      textField_13.setBounds(365, 42, 195, 20);
-	      panel.add(textField_13);
-	      textField_13.setColumns(10);
+	      tfBrojMobilnog = new JTextField();
+	      tfBrojMobilnog.setBounds(10, 318, 195, 20);
+	      panel.add(tfBrojMobilnog);
+	      tfBrojMobilnog.setColumns(10);
 	      
-	      textField_14 = new JTextField();
-	      textField_14.setBounds(365, 73, 195, 20);
-	      panel.add(textField_14);
-	      textField_14.setColumns(10);
+	      tfEmail = new JTextField();
+	      tfEmail.setBounds(10, 349, 195, 20);
+	      panel.add(tfEmail);
+	      tfEmail.setColumns(10);
 	      
-	      textField_15 = new JTextField();
-	      textField_15.setBounds(365, 104, 195, 20);
-	      panel.add(textField_15);
-	      textField_15.setColumns(10);
+	      tfWww = new JTextField();
+	      tfWww.setBounds(10, 380, 195, 20);
+	      panel.add(tfWww);
+	      tfWww.setColumns(10);
 	      
-	      textField_16 = new JTextField();
-	      textField_16.setBounds(365, 135, 195, 20);
-	      panel.add(textField_16);
-	      textField_16.setColumns(10);
+	      tfDatumUpisa = new JTextField();
+	      tfDatumUpisa.setBounds(365, 287, 195, 20);
+	      panel.add(tfDatumUpisa);
+	      tfDatumUpisa.setColumns(10);
 	      
-	      textField_17 = new JTextField();
-	      textField_17.setBounds(365, 166, 195, 20);
-	      panel.add(textField_17);
-	      textField_17.setColumns(10);
+	      tfSmer = new JTextField();
+	      tfSmer.setBounds(365, 318, 195, 20);
+	      panel.add(tfSmer);
+	      tfSmer.setColumns(10);
 	      
 	      JLabel lblNewLabel = new JLabel("Ime");
 	      lblNewLabel.setForeground(new Color(0, 0, 0));
@@ -202,61 +202,69 @@ public class Test {
 	      panel.add(lblDrzavaRodjenja);
 	      
 	      lblImeOca = new JLabel("Ime oca");
-	      lblImeOca.setBounds(215, 231, 140, 14);
+	      lblImeOca.setBounds(570, 14, 140, 14);
 	      panel.add(lblImeOca);
 	      
 	      lblImeMajke = new JLabel("Ime majke");
-	      lblImeMajke.setBounds(215, 257, 140, 14);
+	      lblImeMajke.setBounds(570, 45, 140, 14);
 	      panel.add(lblImeMajke);
 	      
 	      lblUlica = new JLabel("Ulica");
-	      lblUlica.setBounds(215, 290, 140, 14);
+	      lblUlica.setBounds(570, 76, 140, 14);
 	      panel.add(lblUlica);
 	      
 	      lblBoj = new JLabel("Broj");
-	      lblBoj.setBounds(215, 321, 140, 14);
+	      lblBoj.setBounds(570, 107, 140, 14);
 	      panel.add(lblBoj);
 	      
 	      lblMesto = new JLabel("Mesto");
-	      lblMesto.setBounds(215, 350, 140, 14);
+	      lblMesto.setBounds(570, 136, 140, 14);
 	      panel.add(lblMesto);
 	      
-	      textField_20 = new JTextField();
-	      textField_20.setBounds(10, 416, 195, 20);
-	      panel.add(textField_20);
-	      textField_20.setColumns(10);
+	      tfPostanskiBroj = new JTextField();
+	      tfPostanskiBroj.setBounds(365, 202, 195, 20);
+	      panel.add(tfPostanskiBroj);
+	      tfPostanskiBroj.setColumns(10);
 	      
 	      lblDrzava = new JLabel("Drzava");
-	      lblDrzava.setBounds(215, 383, 140, 14);
+	      lblDrzava.setBounds(570, 169, 140, 14);
 	      panel.add(lblDrzava);
 	      
 	      lblPostanskiBroj = new JLabel("Postanski broj");
-	      lblPostanskiBroj.setBounds(215, 419, 140, 14);
+	      lblPostanskiBroj.setBounds(570, 205, 140, 14);
 	      panel.add(lblPostanskiBroj);
 	      
 	      lblNewLabel_1 = new JLabel("Broj telefona");
-	      lblNewLabel_1.setBounds(570, 14, 140, 14);
+	      lblNewLabel_1.setBounds(215, 290, 140, 14);
 	      panel.add(lblNewLabel_1);
 	      
 	      lblBrojMobilnog = new JLabel("Broj mobilnog");
-	      lblBrojMobilnog.setBounds(570, 45, 140, 14);
+	      lblBrojMobilnog.setBounds(215, 321, 140, 14);
 	      panel.add(lblBrojMobilnog);
 	      
 	      lblEmail = new JLabel("Email");
-	      lblEmail.setBounds(570, 76, 140, 14);
+	      lblEmail.setBounds(215, 352, 140, 14);
 	      panel.add(lblEmail);
 	      
 	      lblWwwUri = new JLabel("WWW uri");
-	      lblWwwUri.setBounds(570, 107, 140, 14);
+	      lblWwwUri.setBounds(215, 383, 140, 14);
 	      panel.add(lblWwwUri);
 	      
 	      lblDatumUpisa = new JLabel("Datum upisa");
-	      lblDatumUpisa.setBounds(570, 138, 140, 14);
+	      lblDatumUpisa.setBounds(570, 290, 140, 14);
 	      panel.add(lblDatumUpisa);
 	      
 	      lblSmer = new JLabel("Smer");
-	      lblSmer.setBounds(570, 169, 140, 14);
+	      lblSmer.setBounds(570, 321, 140, 14);
 	      panel.add(lblSmer);
+	      
+	      JButton btnSaveChanges = new JButton("Sacuvaj izmene");
+	      btnSaveChanges.setBounds(556, 377, 140, 23);
+	      panel.add(btnSaveChanges);
+	      
+	      JButton btnPrint = new JButton("Ispisi u datoteku");
+	      btnPrint.setBounds(556, 415, 140, 23);
+	      panel.add(btnPrint);
 	      
 	      mainFrame.addWindowListener(new WindowAdapter() {
 	         public void windowClosing(WindowEvent windowEvent){
